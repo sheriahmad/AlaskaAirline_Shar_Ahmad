f70086278eea26aa9226ba92333dccf7aae1cb2b
Sheheryar Ahmad <sheriahmad@hotmail.com>
2018-03-16 19:22:02 -0700

    Initial Commit

---
+++ AlaskaAirline_Shar_Ahmad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlaskaAirline_Shar_Ahmad
+//
+//  Created by Sheheryar Ahmad on 3/16/18.
+//  Copyright © 2018 Sheheryar Ahmad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
